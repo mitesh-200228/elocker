8dda38f24c7acefa088e6513b945b0185055aeb3
mitesh-200228 <81156492+mitesh-200228@users.noreply.github.com>
2023-11-01 03:22:12 +0530

    latest one

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { R2Explorer } from 'r2-explorer';
+
+export default R2Explorer({
+    readonly: false, basicAuth: [{
+        username: 'info@rippleinfosoft.com',
+        password: 'Ripple123@456'
+    }]
+});
